6733dca7023d9b72f040917148e462fed9ce7a1a
leetusik <leetusik@gmail.com>
2024-09-20 13:48:44 +0900

    Pagniation

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config:
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
 
+    POSTS_PER_PAGE = 25
+
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
